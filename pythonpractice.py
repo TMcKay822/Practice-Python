d0d722fd4c13fa10b44296129986b48f69b5554e
Tiffany McKay <31213229+TMcKay822@users.noreply.github.com>
2020-01-19 17:29:55 -0500

    Update pythonpractice.py

--- pythonpractice.py
+++ pythonpractice.py
@@ -18,3 +18,33 @@ b = int(input("Enter second number: "))
 result = product_or_sum(a,b)
 
 
+#PyNative 2
+#Given a range of numbers. 
+#Iterate from o^th number to the end number and print the sum of the current number
+# and previous number
+def sumNum(a):
+  prev=0
+  for i in range(a):
+    sums = prev +i
+    print(sums)
+    prev = i
+sumNum(10)
+
+
+#PyNative Question 3
+#Accept string from the user and display only those characters which are present at an 
+#even Index
+
+def even_string_index(a):
+  #print("Original string is: ", a)
+  for i in range(len(a)):
+    if i%2==0:
+      print("index", [i],"=", a[i])
+      i+=1
+
+a = input("Enter string: ")
+print("Original string is: ", a)
+print("Printing only even index characters")
+even_string_index(a)
+
+
